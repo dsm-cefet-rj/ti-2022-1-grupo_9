91d5972927d5269c83a7d81fc7841a446a1d129e
martins1st <matheus.martins@saber.parceirosedu.com.br>
2022-07-06 14:08:45 -0300

    alterando pastas

---
+++ frontend/src/routes/usuario/Login.tsx
@@ -0,0 +1,74 @@
+import { STATUS_CODES } from "http";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { ativar, desativar } from "../../core/reducers/loading.redux";
+import * as yup from "yup"
+import SweetAlertService from "../../core/services/sweet-alert.service";
+import Button from "../../shared/components/Button";
+import storeConfig from "../../core/store/store-config";
+import { Logar } from "../../services/auth.service";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { LoginModel } from "../../models/login.model";
+
+export default (props) => {
+
+    const schema = yup.object({
+        email: yup.string().required(),
+        senha: yup.string().required(),
+    }).required();
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({ resolver: yupResolver(schema) });
+
+    const [token, setToken] = useState(null);
+
+    const store: any = useSelector((state) => state);
+
+    let navigate = useNavigate();
+
+    const dispatch = useDispatch<typeof storeConfig.dispatch>()
+
+    async function logar() {
+        dispatch(ativar());
+        const result = await dispatch(Logar(watch() as LoginModel))
+        setToken(result.payload);
+        
+        return;
+    }
+
+    useEffect(() => {
+        if (store.login.status === 'success' && token != null) {
+            localStorage.setItem("token", token);
+            props.setLogged(true);
+            setTimeout(() => navigate("/", { replace: true }), 1800);
+            SweetAlertService.SucessoPersonalizadoComTimer("Login Efetuado com sucesso!", "você será redirecionado em breve");
+        }
+        if(store.login.status !== "loading") dispatch(desativar());
+        
+    },
+        [store.login.status, token]);
+
+    return (<div className="container">
+        <div className="row">
+            <div className="col-12 col-md-6">
+                <label className="form-label" htmlFor="email">Email</label>
+                <input {...register("email")} id="email" type="text" className="form-control" />
+
+            </div>
+
+        </div>
+        <div className="row right-align">
+            <div className="col-12 col-md-6">
+                <label className="form-label" htmlFor="senha">Senha</label>
+                <input {...register("senha")} id="senha" type="password" className="form-control" />
+
+            </div>
+        </div>
+        <div className="row my-4">
+            <div className="col-12 col-md-6">
+                <Button function={logar} title="Logar"></Button>
+            </div>
+        </div>
+    </div>);
+}
---
+++ frontend/src/routes/usuario/Registrar.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect } from "react";
+import Button from "../../shared/components/Button";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import SweetAlertService from "../../core/services/sweet-alert.service";
+import { ConvertService } from "../../core/services/convert.service";
+import { useForm } from "react-hook-form";
+import { RegistrarModel } from "../../models/registrar.model";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { ativar, desativar } from "../../core/reducers/loading.redux";
+import { Registrar } from "../../services/auth.service";
+import storeConfig from "../../core/store/store-config";
+import { FormularioHelper } from "../../core/helpers/formulario.helper";
+
+export default (props) => {
+    const schema = yup.object({
+        email: yup.string().email().required(),
+        nome: yup.string().required(),
+        cpf: yup.string().required(),
+        telefone: yup.string().required(),
+        senha: yup.string().required(),
+        confirmarSenha: yup.string().required(),
+
+    }).required();
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({ resolver: yupResolver(schema) });
+
+    const store: any = useSelector((state) => state)
+    const dispatch = useDispatch<typeof storeConfig.dispatch>()
+    let navigate = useNavigate();
+    async function Criar() {
+        if (FormularioHelper.ExisteErro(errors)) return SweetAlertService.ErroformularioInvalido(errors);
+        
+        dispatch(ativar())
+        dispatch(Registrar(watch() as RegistrarModel))
+    }
+
+    useEffect(() => {
+        if (store.registrar.status == "success") {
+            SweetAlertService.SucessoPersonalizadoComTimer("Registrado com Sucesso", "você será redirecionado em breve!");
+            setTimeout(() => navigate("/", { replace: true }), 1800);
+            dispatch(desativar());   
+        }
+        else if (store.registrar.status !== "loading") dispatch(desativar());   
+    }, [store.registrar.status])
+
+
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit(() => {})}>
+                <div className="row mt-2">
+                    <div className="col-12 col-md-6">
+                        <label className="form-label" htmlFor="nome">Nome Completo</label>
+                        <input id="nome" {...register("nome")} type="text" className="form-control" />
+
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <label className="form-label" htmlFor="email">Email</label>
+                        <input id="email" type="text" {...register("email")} className="form-control" />
+                    </div>
+                </div>
+                <div className="row mt-2">
+                    <div className="col-12 col-md-6">
+                        <label className="form-label" htmlFor="cpf">CPF</label>
+                        <input id="cpf" type="text" {...register("cpf")} className="form-control" />
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <label className="form-label" htmlFor="telefone">Telefone</label>
+                        <input id="telefone" type="text" {...register("telefone")} className="form-control" />
+                    </div>
+                </div>
+                <div className="row mt-2">
+                    <div className="col-12 col-md-6">
+                        <label className="form-label" htmlFor="Senha">Senha</label>
+                        <input id="Senha" type="password" {...register("senha")} className="form-control" />
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <label className="form-label" htmlFor="confirmarSenha">Confirmar Senha</label>
+                        <input id="confirmarSenha" {...register("confirmarSenha")} type="password" className="form-control" />
+                    </div>
+                </div>
+                <div className="row my-4 center-align">
+                    <div className="col-12">
+                        <Button type="submit" function={Criar} title="Registrar"></Button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+}
